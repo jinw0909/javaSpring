f7991c8a42af3f330986ee580b35838e5a89ab5a
Jinwoo Chang <>
2021-07-20 12:22:37 +0900

    [장진우] - java spring practice second commit / added minor comments for Data class

--- src/main/java/com/jinw0909/spring/lesson01/Data.java
+++ src/main/java/com/jinw0909/spring/lesson01/Data.java
@@ -4,17 +4,23 @@ public class Data {
 	// id, 이름
 	private int id;
 	private String name;
+	
+	// getter setter 추가
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	
 }
