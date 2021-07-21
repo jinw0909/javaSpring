286b51809f583a4982ad35fb62d0fff7cceca308
Jinwoo Chang <>
2021-07-21 11:22:01 +0900

    파일 추가

---
+++ src/main/java/com/jinw0909/spring/lesson01/Ex02Controller.java
@@ -0,0 +1,12 @@
+package com.jinw0909.spring.lesson01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Ex02Controller {
+	@RequestMapping("/lesson01/ex02")
+	public String view() {
+		return "lesson01/ex02";
+	}
+}
